92c555ec4b42d3611e6ed7ed1e4ea0c06a52774e
高雄辉 <gaoxionghui@sunmi.com>
2019-02-13 16:52:59 +0800

    go study

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "./location"
+
+func main() {
+	//hello world go!
+	//fmt.Println("hello, world!")
+
+	//web demo
+	//web.Demo()
+
+	//mysql demo
+	//db.Demo()
+	location.InsertLocation()
+}
