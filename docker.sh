86ae874a033ee1b1dad213dfeae9c657fd07559c
devil <15238677147@139.com>
2019-02-20 13:52:55 +0800

    修改构建脚本

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 cd src/LocationServer
 CGO_ENABLED=0 go get
 cd ../../
-docker build -t locationserver:v1 .
-docker run --rm -it -d -p 8080:8080 locationserver
+docker build -t locationserver:V1 .
+docker run --rm -it -d -p 8080:8080 locationserver:V1
